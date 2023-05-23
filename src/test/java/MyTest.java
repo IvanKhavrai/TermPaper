b03c8f160355483cf0fc30135013470fe7ef4877
IvanKhavrai <104062131+IvanKhavrai@users.noreply.github.com>
2023-05-23 23:03:23 +0200

    preparation

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -4,8 +4,8 @@ import org.testng.Assert;
 public class MyTest {
     @Test
     public void testSomething() {
-        int expected = 10;
-        int actual = 5 + 5;
+        int expected = 12;
+        int actual = 6 + 6;
 
         Assert.assertEquals(expected, actual);
     }
