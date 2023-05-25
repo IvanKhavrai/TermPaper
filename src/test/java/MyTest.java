c2229d9152ea2ea545b23a26f728802d4e7a145f
IvanKhavrai <104062131+IvanKhavrai@users.noreply.github.com>
2023-05-25 16:18:11 +0200

    add mistake into MyTest

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 public class MyTest {
     @Test
     public void testSomething() {
-        int expected = 12;
+        int expected = 13;
         int actual = 6 + 6;
 
         Assert.assertEquals(expected, actual);
